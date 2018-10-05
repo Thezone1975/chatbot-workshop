22c3a43861613bbc90463ebb3338333a9459fe9e
Antonio Cangiano <acangiano@gmail.com>
2018-10-05 10:14:21 -0400

    Add chatbot course image

--- src/pages/lab-1.js
+++ src/pages/lab-1.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import {Link} from 'gatsby'
-import { Button } from 'reactstrap';
+import courseImage from '../images/how-to-build-a-chatbot.png'
 
 import Layout from '../components/layout'
 
@@ -23,7 +22,7 @@ const SecondPage = () => (
       To create chatbots, we'll use IBM Watson Assistant, an easy to use and powerful chatbot service offered on the IBM Cloud. So we need to make sure that you have an account with IBM Cloud.
     </p>
     <ol>
-      <li>Register with IBM Cloud by clicking on <a href="https://cocl.us/CB0103EN_BM" target="_blank" rel="noopener">this link</a>.</li>
+      <li>Register with IBM Cloud by clicking on <a href="https://cocl.us/CB0103EN_BM" target="_blank" rel="noopener noreferrer">this link</a>.</li>
       <li>You'll be asked to log in if you already have an account with IBM.com. If you do, log in. If you don't, click on Create an account and proceed with the creation of an account with your professional email address (or personal if you prefer).</li>
       <li>If you created an account in the previous step, you'll be asked to verify your email. Check your inbox for the verification email (and for good measure, be sure to check your spam folder as well, if you don't see the email after a few minutes).</li>
       <li>At this point you'll be logged in with your IBM ID. The next step is to sign up for IBM Cloud, if you haven't done so already in the past. If you don't have an IBM Cloud account yet, you'll be presented with a page asking you to sign up. Click on the Sign Up button presented to you on the page. If clicking it does nothing, click the log out link available on the page, log back in with your IBM ID, and then try again.</li>
@@ -33,7 +32,7 @@ const SecondPage = () => (
       <li>You're now logged in and should see your (empty) dashboard.</li>
     </ol>
     <p>
-      You can access this page at any time by logging in on the ibm.com/cloud site, but I highly recommend that you just bookmark the <a href="https://console.bluemix.net/dashboard/apps/" target="_blank" rel="noopener">dashboard page</a> instead. It will give you quick access to seeing and managing all the resources you created inside of your IBM Cloud account.
+      You can access this page at any time by logging in on the ibm.com/cloud site, but I highly recommend that you just bookmark the <a href="https://console.bluemix.net/dashboard/apps/" target="_blank" rel="noopener noreferrer">dashboard page</a> instead. It will give you quick access to seeing and managing all the resources you created inside of your IBM Cloud account.
     </p>
 
     <h3>Task 2: Create an instance of Watson Assistant</h3>
@@ -51,7 +50,11 @@ const SecondPage = () => (
     </ol> 
 
     <h3>Task 3: Enroll in the Chatbot Course</h3>
-    <p>Last year, I created a chatbot course that's been taken by about 50,000 people around the world. You can think of this workshop as a more in depth, up to date, and interactive version of my online course available on <a href="https://cognitiveclass.ai" target="_blank" rel="noopener">CognitiveClass.ai</a>. You don't need to take the course, since you're getting the in-person workshop which covers the same material and more; however, you should still sign up for Cognitive Class and enroll in my course. There are a few reason for this task:</p>
+    <p><a href="https://cognitiveclass.ai/courses/how-to-build-a-chatbot/" target="_blank" rel="noopener noreferrer"><img src={courseImage} alt="Chatbot Course banner" style={{float: "right", margin: "20px"}} /></a>Last year, I created a chatbot course that's been taken by about 50,000 people around the world. You can think of this workshop as a more in depth, up to date, and interactive version of my online course available on <a href="https://cognitiveclass.ai" target="_blank" rel="noopener noreferrer">CognitiveClass.ai</a>.
+    </p>
+    <p>You don't need to take the course, since you're getting the in-person workshop which covers the same material and more; however, you should still sign up for Cognitive Class and enroll in my course now.</p>
+    
+    <p>There are a few reason for enrolling:</p>
     <ul>
       <li>Being enrolled in the course will enable you to take the quizzes and final exam. A passing grade will entitle you to a badge backed by IBM, to attest to your ability to create chatbots. This badge, digitally issued by Acclaim, can then be shared on your LinkedIn profile, other social media sites (a little bragging won't hurt anyone), on your CV/résumé, etc.</li>
       <li>Upon passing the exam, you'll also receive a digital certificate of accomplishment from Cognitive Class, which you could print if you wished.</li>
@@ -60,7 +63,7 @@ const SecondPage = () => (
       <li>Once you're registered with Cognitive Class, you can take many other courses on Data Science, AI, Containers, Blockchain, and more if you're interested. All for free and at your own pace.</li>
     </ul>
     <p>
-      To register with Cognitive Class and enroll in the Build Your Own Chatbot course, click on the Start the Free Course button on the <a href="https://cognitiveclass.ai/courses/how-to-build-a-chatbot/" target="_blank" rel="noopener">course page</a>. You'll be asked to login (if you already have a Cognitive Class account) or register with Cognitive Class and then verify your email (as usual with most services). On the verification page, when you click the activation link sent to you by email, you'll be prompted with the aforementioned US$1200 special offer. You can safely ignore it for now. Later in the workshop, we'll cover how to obtain the free WordPress testing environment inside of the course you just enrolled in.
+      To register with Cognitive Class and enroll in the Build Your Own Chatbot course, click on the Start the Free Course button on the <a href="https://cognitiveclass.ai/courses/how-to-build-a-chatbot/" target="_blank" rel="noopener noreferrer">course page</a>. You'll be asked to login (if you already have a Cognitive Class account) or register with Cognitive Class and then verify your email (as usual with most services). On the verification page, when you click the activation link sent to you by email, you'll be prompted with the aforementioned US$1200 special offer. You can safely ignore it for now. Later in the workshop, we'll cover how to obtain the free WordPress testing environment inside of the course you just enrolled in.
     </p>
     <p>
       This concludes your very first lab. On the next one, we'll start getting our hands dirty by building our first chatbot components.
