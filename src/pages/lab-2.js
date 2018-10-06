04b6f73c59bca47b553bbffbbbef25aaf48dd982
Antonio Cangiano <acangiano@gmail.com>
2018-10-05 21:44:27 -0400

    Add beginning of second lab

--- src/pages/lab-2.js
+++ src/pages/lab-2.js
@@ -7,11 +7,10 @@ import Layout from '../components/layout'
 
 const Lab2 = () => (
   <Layout>
-    <h2>Lab 2: Working with Intents and Entities</h2>
+    <h2>Lab 2: Working with Intents</h2>
     <p>
-      Welcome to your first hands-on lab. I'm sure you're eager to get started.
-      Before we can begin to actually build chatbots, however, we'll need to
-      take care of a couple of prerequisites for this workshop.
+      Great job on completing the first lab. We're now ready to tackle the first
+      and most important component of a chatbot, Intents.
     </p>
 
     <h3>What you will accomplish</h3>
@@ -25,43 +24,139 @@ const Lab2 = () => (
       </li>
       <li>Import and export intents via CSV files;</li>
       <li>
-        Test intents and entities in the{' '}
-        <span className="instruction">Try it</span> panel.
+        Test intents in the <span className="instruction">Try it</span> panel.
       </li>
-      <li>Take your second graded quiz.</li>
     </ol>
 
     <h3>Task 1: Create a workspace for your chatbot</h3>
+    <p>
+      This lab is all about intents, but in order to work with them, we'll need
+      to create a workspace which will contain said intents (along with entities
+      and the dialog).
+    </p>
+    <p>
+      Although it's possible to create elaborate chatbots that use multiple
+      workspaces, it is common to have just one workspace per chatbot. In other
+      words, for now you can simply{' '}
+      <strong>think of your workspace as your chatbot</strong>.
+    </p>
+    <ol>
+      <li>
+        From the <span className="instruction">Workspaces</span> page of your
+        Watson Assistant instance [
+        <span
+          style={{
+            color: 'blue',
+            fontWeight: 'bold',
+          }}
+          href="#"
+          id="no-bookmark"
+        >
+          Tip
+        </span>
+        ]
+        <UncontrolledTooltip placement="top" target="no-bookmark">
+          If you didn't bookmark your{' '}
+          <span className="instruction">Workspaces</span> page in the previous
+          lab, you can find your way back by visiting your dashboard and
+          clicking on the Watson Assistant service you created.
+        </UncontrolledTooltip>
+        ,{' '}
+        <strong>
+          click on the <span className="instruction">Create</span> button
+        </strong>{' '}
+        within the <span className="instruction">Create a new workspace</span>{' '}
+        tile. [
+        <span
+          style={{
+            color: 'blue',
+            fontWeight: 'bold',
+          }}
+          href="#"
+          id="ignore-customer-service"
+        >
+          Note
+        </span>
+        ]
+        <UncontrolledTooltip placement="top" target="ignore-customer-service">
+          You'll also see a Customer Service sample that you can create by
+          clicking <span className="instruction">Edit sample</span>. Ignore it
+          for now, as we'll take a look at it later in the labs.
+        </UncontrolledTooltip>
+      </li>
+      <li>
+        You'll be offered to enter a <span className="instruction">Name</span>{' '}
+        and an optional <span className="instruction">Description</span>. Enter{' '}
+        <code>Flower Shop Chatbot</code> or whatever you prefer for the name.
+        For the description, feel free to add{' '}
+        <code>A chatbot for a flower shop chain.</code> or something similar.
+      </li>
+      <li>
+        We'll create the chatbot in English, but{' '}
+        <strong>which other languages are also supported?</strong> Find out from
+        this window.
+      </li>
+      <li>
+        Ensuring <span className="instruction">English (U.S.)</span> is
+        selected,{' '}
+        <strong>
+          click on the <span className="instruction">Create</span> button
+        </strong>{' '}
+        to generate the empty workspace for your chatbot.
+      </li>
+      <li>
+        Practice going back and forth between{' '}
+        <span className="instruction">Workspaces</span> and the workspace you
+        just created (e.g., <code>Flower Shop Chatbot</code>
+        ).
+      </li>
+    </ol>
+
     <h3>Task 2: Create, modify, and delete intents</h3>
+
+    <p>
+      Upon creation of the workspace, you'll find yourself on the{' '}
+      <span className="instruction">Intents</span> section of your workspace.
+      Here you can add intents in several ways. In this task, we'll focus on the
+      most straightfoward way. That is, manually adding intents.
+    </p>
+
+    <ol>
+      <li>
+        <strong>
+          Click on the <span className="instruction">Add intent</span> button.
+        </strong>
+      </li>
+      <li>
+        What happens if you try to call the intent <code>#greeting us</code>{' '}
+        with a space in the name?
+      </li>
+      <li>
+        <strong>
+          Define a <code>#greetings</code> intent
+        </strong>
+        . Optionally add an appropriate description as well, and then{' '}
+        <strong>
+          click <span className="instruction">Create intent</span>
+        </strong>
+        .
+      </li>
+      <li>
+        You'll be prompted to create some examples to train Watson on the
+        concept of greetings. Try examples such as <code>hello</code>,
+        <code>hi</code>, <code>hey</code>, <code>good morning</code>,
+        <code>good afternoon</code>, and so on. You don't need to go crazy,
+        especially on such a simple intent, but you should always{' '}
+        <strong>include at least 5 examples</strong>. Ideally, more.
+      </li>
+    </ol>
+
     <h3>Task 3: Add intents from the Content Catalog</h3>
+
     <h3>Task 4: Import intents from a CSV file</h3>
+
     <h3>Task 5: Test intents in the Try it panel</h3>
 
-    <p>
-      To create chatbots, we'll use IBM Watson Assistant, an easy to use and
-      powerful chatbot service offered on the IBM Cloud. So we need to make sure
-      that you have an account with the IBM Cloud.
-    </p>
-
-    <h3>Task 6: Take the Module 2 Quiz</h3>
-    <p>
-      Head over to the{' '}
-      <a
-        href="https://courses.cognitiveclass.ai/courses/course-v1:CognitiveClass+CB0103EN+v1/courseware/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Build Your Own Chatbot
-      </a>{' '}
-      course and{' '}
-      <strong>
-        find the <span className="instruction">Graded Review Questions</span>{' '}
-        inside Module 2. Take the quiz
-      </strong>
-      , just like you did in the previous module. As a reminder, there are no
-      trick questions, so go with the answer that appears to be most logical to
-      you.
-    </p>
     <blockquote>
       Having completed this lab, you're well on your way to becoming a buddying
       chatbot builder. Intents really are the most fundamental part of a
